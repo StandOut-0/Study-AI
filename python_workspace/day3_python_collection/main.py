2e3b4da2440cce6cd9ec371d8dd38601de223258
playdata\playdata2 <sanghee2411@gmail.com>
2026-04-30 11:13:08 +0900

    [STUDY] Day3 - python_collection - main - import와 from

---
+++ python_workspace/day3_python_collection/main.py
@@ -0,0 +1,12 @@
+# import my_collections.test_list.list_sample as ls
+from my_collections.test_list.list_sample import make_list
+
+
+
+if __name__ == "__main__":
+    print("start")
+    
+    # print(ls.make_list())
+    print(make_list())
+
+    print("end")
---
+++ python_workspace/day3_python_collection/my_collections/test_list/list_sample.py
@@ -0,0 +1,4 @@
+# collections\test_list\list_sample.py
+
+def make_list():
+    return [1, 2, 3, 4, 5]
